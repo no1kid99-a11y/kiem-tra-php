abcefb12dfd1335c3755485c834b28538496b835
no1kid99-a11y <no1kid99@gmail.com>
2026-09-07 14:33:33 +0700

    Create bai5.php

---
+++ bai5.php
@@ -0,0 +1,13 @@
+<?php
+/*
+BÀI 5: Số chẵn in đậm, số lẻ in thường
+*/
+
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 2 == 0) {
+        echo "<b>$i</b> ";
+    } else {
+        echo "$i ";
+    }
+}
+?>
